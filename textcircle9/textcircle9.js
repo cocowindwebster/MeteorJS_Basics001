c458a92cc83e8e9820eb6bfe4665e4311f992028
Cocowind Webster <cocowindwebster@gmail.com>
2016-02-18 23:52:04 -0800

    display who is editing

---
+++ textcircle9/textcircle9.js
@@ -0,0 +1,79 @@
+this.Documents = new Mongo.Collection("documents");
+EditingUsers = new Mongo.Collection("editingUsers");
+
+if (Meteor.isClient) {
+    Template.editor.helpers({
+        docid:function() {
+            var doc = Documents.findOne();
+            if (doc) {
+                return doc._id;
+            } else {
+                return undefined;
+            }
+        },
+        config:function() {
+            return function(editor) {
+                editor.setOption("lineNumber", true);
+                editor.setOption("mode", "html");
+                editor.on("change", function(cm_editor, info){
+                    console.log(cm_editor.getValue());
+                    $("#viewer_iframe").contents().find("html").html(cm_editor.getValue());
+                    Meteor.call("addEditingUser");
+                } );
+            }   
+        }
+
+    });
+
+
+    Template.editingUsers.helpers({
+        users:function() {
+            var doc, eusers, users;
+            doc = Documents.findOne();
+            if (!doc) {
+                return;
+            }
+            eusers = EditingUsers.findOne({docid : doc._id});
+            if (!eusers) {
+                return;
+            }
+            users = new Array();
+            var i = 0;
+            for (var user_id in eusers.users) {
+                console.log("Adding User");
+                console.log(eusers.users[user_id]);
+                users[i] = eusers.users[user_id];
+                i++;
+            }
+            return users;
+        }     
+    });
+}
+
+if (Meteor.isServer) {
+    Meteor.startup(function () {
+      if (!Documents.findOne()) {
+          Documents.insert({title : "my new documents"});
+      }
+  });
+}
+
+Meteor.methods({
+    addEditingUser:function(){
+        var doc, user, eusers;
+        doc = Documents.findOne();
+        if (!doc) {return;} //no doc, so return
+        if (!this.userId) {return;} //no user, so return
+        user = Meteor.user().profile;
+        eusers = EditingUsers.findOne({docid:doc._id});
+        if (!eusers) {
+            eusers = {
+                docid:doc._id,
+                users:{},
+            }; 
+        }
+        user.lastEdit = new Date();
+        eusers.users[this.userId] = user;
+        EditingUsers.upsert({_id:eusers._id},eusers); //upsert : insert only if it is not existed.
+    }
+}) 
