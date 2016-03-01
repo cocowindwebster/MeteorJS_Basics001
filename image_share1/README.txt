4bafaa896c32310ceb011ee7f913b47d439c4ac3
Cocowind Webster <cocowindwebster@gmail.com>
2016-02-29 21:31:47 -0800

    responding to user input

--- image_share1/README.txt
+++ image_share1/README.txt
@@ -2,3 +2,4 @@
 2. Create a single statis image, this time, used javascript variables to assign image locations and alt property, then call the javascript variables from the HTML template.
 3. In html Template, iterate through variables, which are passed from javascript 
 4. Add bootstrap package, and put three images in the bootstrap grid, demonstrating bootstrap grid responsive feature.
+5. Responding to use input
--- image_share1/image_share1.js
+++ image_share1/image_share1.js
@@ -14,12 +14,23 @@ if (Meteor.isClient) {
         image_alt : "All large hot air balloons"
     }
     ];
-    //Template helper functions provide data for Template.
+    
+    //bind data to this Template, named "my_image"
     Template.my_image.helpers({images:image_data}); 
     // instead of passing an ARRAY called image_data, pass a property that 
     // has KEY "images", and VALUE "image_data"
     // with this property structure, html TEMPLATE can iterate
     // looks like a comma ending a line, can be either omitted or kept
+
+    Template.my_image.events({
+        //bind event lisenter to this Template, named "my_image"
+        'click .js-image':function(event) {
+             alert("hello");
+             console.log(event);
+             console.log(event.originalEvent.clientX);
+             $(event.target).css("width", "50px");
+        } 
+    });
 
 }
 
