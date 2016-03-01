397c7ddad3cf282001293facf5c9434c75ee66c8
Cocowind Webster <cocowindwebster@gmail.com>
2016-02-29 20:35:08 -0800

    iterate through variables, which are passed from javascript

--- image_share1/README.txt
+++ image_share1/README.txt
@@ -1,2 +1,3 @@
 1. create  a single static image, simply to demonstrate hwo template is used to manipulate the html language.
 2. create a single statis image, this time, used javascript variables to assign image locations and alt property, then call the javascript variables from the HTML template.
+3. in html Template, iterate through variables, which are passed from javascript 
--- image_share1/image_share1.js
+++ image_share1/image_share1.js
@@ -1,11 +1,24 @@
 if (Meteor.isClient) {
     console.log("This is at client")
-    var image_data = {
+    var image_data = [
+    {
         image_source : "1.png",
-        image_alt : "a large hot air balloon"
+        image_alt : "One large hot air balloon"
+    },
+    {
+        image_source : "2.png",
+        image_alt : "Two large hot air balloons"
+    },
+    {
+        image_source : "3.png",
+        image_alt : "All large hot air balloons"
     }
+    ];
     //Template helper functions provide data for Template.
-    Template.my_image.helpers(image_data); 
+    Template.my_image.helpers({images:image_data}); 
+    // instead of passing an ARRAY called image_data, pass a property that 
+    // has KEY "images", and VALUE "image_data"
+    // with this property structure, html TEMPLATE can iterate
     // looks like a comma ending a line, can be either omitted or kept
 
 }
