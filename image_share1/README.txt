ecdb9078da9f86c89affc436199f3a0a17664f29
Cocowind Webster <cocowindwebster@gmail.com>
2016-02-29 20:22:19 -0800

    assign image property to javascript vars, and use those javascript vars in html Template

--- image_share1/README.txt
+++ image_share1/README.txt
@@ -1,1 +1,2 @@
 1. create  a single static image, simply to demonstrate hwo template is used to manipulate the html language.
+2. create a single statis image, this time, used javascript variables to assign image locations and alt property, then call the javascript variables from the HTML template.
--- image_share1/image_share1.js
+++ image_share1/image_share1.js
@@ -1,23 +1,18 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+    console.log("This is at client")
+    var image_data = {
+        image_source : "1.png",
+        image_alt : "a large hot air balloon"
     }
-  });
+    //Template helper functions provide data for Template.
+    Template.my_image.helpers(image_data); 
+    // looks like a comma ending a line, can be either omitted or kept
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+    console.log("This is at server")
   });
 }
